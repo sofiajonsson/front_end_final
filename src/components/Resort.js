0bd073bdc367eec13c1600004a1c73408cfe0f47
Sofia <jonssonsofia93@gmail.com>
2019-06-11 12:34:43 -0700

    seeded models are on pages

--- src/components/Resort.js
+++ src/components/Resort.js
@@ -5,64 +5,64 @@ class Resort extends Component{
 		super(props)
 			this.state={}
 		}
+
+		// <tr>
+		// 	<td>
+		// 	</td>
+		// 	<td>
+		// 	Elevation:
+		// 	</td>
+		// 	<td>
+		// 	Skiiable Terrain:
+		// 	</td>
+		// 	<td>
+		// 	Beginner Runs:
+		// 	</td>
+		// 	<td>
+		// 	Intermediate Runs:
+		// 	</td>
+		// 	<td>
+		// 	Expert Runs:
+		// 	</td>
+		// 	<td>
+		// 	Total Lifts:
+		// 	</td>
+		// 	<td>
+		// 	Ticket Price:
+		// 	</td>
+		// 	</tr>
+
 	render() {
-		console.log(this.props.resort);
 		return(
 			<div>
 				<div className="card-body">
 					<h3 className="card-title">{this.props.resort.title}</h3>
+
 					<table className="forecast-table">
+
 						<tbody>
-						<tr>
-							<td>
-							</td>
-							<td>
-							Elevation:
-							</td>
-							<td>
-							Skiiable Terrain:
-							</td>
-							<td>
-							Beginner Runs:
-							</td>
-							<td>
-							Intermediate Runs:
-							</td>
-							<td>
-							Expert Runs:
-							</td>
-							<td>
-							Total Lifts:
-							</td>
-							<td>
-							Ticket Price:
-							</td>
-							</tr>
-						<tr>
-							<td>
-							<img src={`https://www.skiresort.info/${this.props.resort.img}`} alt="ski-resort-img"/>
-							</td>
-							<td>
-							<h5> {this.props.resort.elevation_gain}</h5>
-							</td>
-							<td>
-							<h5> {this.props.resort.skiiable_terrain}}</h5>
-							</td>
-							<td>
-							<h5> {this.props.resort.beginner}}</h5>
-							</td>
-							<td>
-							<h5>{this.props.resort.intermediate}}</h5>
-							</td>
-							<td>
-							<h5>{this.props.resort.expert}}</h5>
-							</td>
-							<td>
-							<h5>{this.props.resort.total_lifts}}</h5>
-							</td>
-							<td>
-							<h5> {this.props.resort.ticket_price}}</h5>
-							</td>
+							<tr>
+								<td>
+									<img src={`https://www.skiresort.info/${this.props.resort.img}`} alt="ski-resort-img"/>
+								</td>
+								<td>
+									<h6>Elevation:</h6>
+									<h6>Skiiable Terrain:</h6>
+									<h6>Beginner Runs:</h6>
+									<h6>Intermediate Runs:</h6>
+									<h6>Expert Runs:</h6>
+									<h6>Total Lifts:</h6>
+									<h6>Ticket Price:</h6>
+								</td>
+								<td>
+									<h6>{this.props.resort.elevation_gain}</h6>
+									<h6>{this.props.resort.skiiable_terrain}</h6>
+									<h6>{this.props.resort.beginner}</h6>
+									<h6>{this.props.resort.intermediate}</h6>
+									<h6>{this.props.resort.expert}</h6>
+									<h6>{this.props.resort.total_lifts}</h6>
+									<h6>{this.props.resort.ticket_price}</h6>
+								</td>
 							</tr>
 						</tbody>
 					</table>
