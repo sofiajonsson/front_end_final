e175c6311896b3169f7a7be5791062c1dcc06921
Sofia <jonssonsofia93@gmail.com>
2019-06-20 11:43:27 -0700

    Home page looks better

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,8 +7,23 @@ class Home extends Component {
 
 		return(
 			<div id="home-image" >
-            <img src='https://181ge72mb8rnbx7z1k119thi-wpengine.netdna-ssl.com/wp-content/uploads/2014/07/Tetons-BANG2-web-e1476286636877.jpg'></img>
+			<img src='https://181ge72mb8rnbx7z1k119thi-wpengine.netdna-ssl.com/wp-content/uploads/2014/07/Tetons-BANG2-web-e1476286636877.jpg'></img>
+			 <div className="centered">Snow Tracker
+			 <br />
+			<br />
+			<br />
+			<br />
+			<br />
+			 <p className="home-p-tag">Keep up with your favorite resorts</p>
+			 <button position="center" class="ui button">Check Out Resorts </button>
+			<br />
+			<br />
+
+			<br />
 			</div>
+
+		</div>
+
 		)
 	}
 }
